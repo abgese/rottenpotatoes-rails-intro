89b924f8a3c4767e0c7545733626ae37b309fc4c
abgese <abraham.g.sebastian@gmail.com>
2020-02-18 00:09:16 +0000

    Final commit for part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort])
+    @movies = Movie.all.order(params[:sort])
+    @sort_col = params[:sort]
   end
 
   def new
